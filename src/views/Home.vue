489160cd637dd09e0366495022ef5c95e6ccce75
Kevin_QQ <ezhqing@163.com>
2018-06-30 06:45:57 +0800

    offline

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo.png">
+    <img src="../assets/pwa.png">
+    <img src="../assets/flask.png">
+    <HelloWorld msg="Vue.js + PWA on Flask backend"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ static/precache-manifest.e4d1e98c8ff6e9f1a67eb5e998838842.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "217e7126a4224fbcbdf5",
+    "url": "/js/chunk-vendors.217e7126.js"
+  },
+  {
+    "revision": "33f285f757c1583bd0ec",
+    "url": "/js/app.33f285f7.js"
+  },
+  {
+    "revision": "c136803f0c16c86be0b545a64f46d33f",
+    "url": "/"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "33f285f757c1583bd0ec",
+    "url": "/css/app.5024d133.css"
+  }
+];
