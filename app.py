d3618c459a3bf8f869d75f94773ad87066725f12
kevinqqnj <ezhqing@gmail.com>
2018-07-13 17:18:14 +0800

    template

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__, static_url_path='')
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/pages/<str:page>')
+def pages():
+    return render_template(f'/pages/{page}')
