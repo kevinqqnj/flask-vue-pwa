41aeb34f63e36e0178d6dc97c1d36c12ab023a4c
Kevin_QQ <ezhqing@163.com>
2018-07-13 18:25:20 +0800

    dist

---
+++ dist/precache-manifest.a7351e35b98d26433e7d099ecdec0b49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "217e7126a4224fbcbdf5",
+    "url": "/js/chunk-vendors.217e7126.js"
+  },
+  {
+    "revision": "1dafa64a88dee9dcf245",
+    "url": "/js/app.1dafa64a.js"
+  },
+  {
+    "revision": "55704df5f51802f18a03485b6c860929",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6693b1c68472145e089904e5df6c74fb",
+    "url": "/img/pwa.6693b1c6.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b228671d7ec585c18c6d8a5637273e04",
+    "url": "/img/flask.b228671d.png"
+  },
+  {
+    "revision": "1dafa64a88dee9dcf245",
+    "url": "/css/app.6036d612.css"
+  }
+];
---
+++ dist/service-worker.js
@@ -0,0 +1,91 @@
+importScripts("/precache-manifest.a7351e35b98d26433e7d099ecdec0b49.js", "https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox-sw.js");
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+
+    workbox.core.setCacheNameDetails({ prefix: "flask-vue-pwa" });
+
+    /**
+     * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+     * requests for URLs in the manifest.
+     * See https://goo.gl/S9QRab
+     */
+    self.__precacheManifest = [
+        { 'url': '/pages/offline.html' },
+        { 'url': '/pages/404.html' },
+    ].concat(self.__precacheManifest || []);
+    workbox.precaching.suppressWarnings();
+    workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+
+    workbox.routing.registerRoute(
+        /(.*)articles(.*)\.(?:png|gif|jpg)/,
+        workbox.strategies.cacheFirst({
+            cacheName: 'images-cache',
+            plugins: [
+                new workbox.expiration.Plugin({
+                    maxEntries: 50,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                })
+            ]
+        })
+    );
+
+    // workbox.routing.registerRoute(/(.*)article(.*)\.html/, args => {
+    //     return articleHandler.handle(args).then(response => {
+    //         if (!response) {
+    //             return caches.match('pages/offline.html');
+    //         } else if (response.status === 404) {
+    //             return caches.match('pages/404.html');
+    //         }
+    //         return response;
+    //     });
+    // });
+
+    const articleHandler = workbox.strategies.networkFirst({
+        cacheName: 'articles-cache',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 50,
+            })
+        ]
+    });
+
+    workbox.routing.registerRoute(/article\/(.*)/, args => {
+        return articleHandler.handle(args).then(response => {
+            if (!response) {
+                return caches.match('pages/offline.html');
+            } else if (response.status === 404) {
+                return caches.match('pages/404.html');
+            }
+            return response;
+        });
+    });
+
+    const postHandler = workbox.strategies.cacheFirst({
+        cacheName: 'posts-cache',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 50,
+            })
+        ]
+    });
+
+    workbox.routing.registerRoute(/post\/.(.*)/, args => {
+        console.log('test');
+        return postHandler.handle(args).then(response => {
+                if (response.status === 404) {
+                    return caches.match('pages/404.html');
+                }
+                return response;
+            })
+            .catch(function() {
+                return caches.match('pages/offline.html');
+            });
+    });
+
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+}
+
+
